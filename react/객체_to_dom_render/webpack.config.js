089f3cc2846be70f1416151bc60e80d01a8018c6
gaeun <gaeun2937@gmail.com>
2022-12-13 23:50:25 +0900

    2022-12-13

---
+++ react/객체_to_dom_render/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },  
+    module: {
+
+    },
+    plugins: [
+
+    ]
+}
