c9429107dc6e3995564bbe6a57a9d7c08eefe9e9
gaeun <gaeun2937@gmail.com>
2023-01-02 22:36:24 +0900

    클래스 컴포넌트 만들기

--- react/src/app_v3.js
+++ react/src/app_v3.js
@@ -2,12 +2,11 @@
 
 // createElement가 코드 상에서 쓰고 있지 않지만 반드시 포함 시켜 줘야 runtime 오류 발생하지 않음
 // jsx가 createElement함수 호출 구문으로 바뀌어 있기 때문에
-import {createDOM, createElement, render, Component} from './react';
+import {createDOM, createElement, render} from './react';
 
-class Title extends Component {
-    render(){
-        return <h1>{ this.props.children }</h1>;
-    }
+
+function Title(props){
+    return <h1>{ props.children }</h1>;
 }
 
 function Item(props){
@@ -16,7 +15,7 @@ function Item(props){
 }
 
 const App = () => <p>
-    <Title>React 클래스 컴포넌트 만들기입니다.</Title>
+    <Title>React 만들기입니다.</Title>
     <ul>
         <Item color="red">첫 번째 아이템</Item>
         <Item color="blue">두 번째 아이템</Item>
--- react/src/react_v3.js
+++ react/src/react_v3.js
@@ -1,9 +1,3 @@
-export class Component{
-    constructor(props){
-        this.props = props;
-    }
-}
-
 export function createDOM(node){
     if(typeof node == 'string'){
         return document.createTextNode(node);
@@ -20,33 +14,19 @@ export function createDOM(node){
     return element;
 }
 
-function makeProps(props, children){
-    return {
-        ...props,
-        children: children.length === 1 ? children[0]: children,
-    };
-}
-
 // ...children : 가변인자로 받음(배열로 들어가게 됨)
 export function createElement(tag, props, ...children){
     props = props || {};
 
     if(typeof tag === 'function'){
-        // Component를 상속받은 class인지 확인하여 클래스 여부 체크
-        if(tag.prototype instanceof Component){
-            const instance = new tag(makeProps(props, children));
-            return instance.render();
-
+        if(children.length > 0){
+            return tag({
+                ...props,
+                children: children.length === 1 ? children[0]: children,
+            })
         }else{
-            if(children.length > 0){
-                return tag(makeProps(props, children));
-
-            }else{
-                return tag(props);
-            }
+            return tag(props);
         }
-
-        
         
     }else{
         return { tag, props, children };
